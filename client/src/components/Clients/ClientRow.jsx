437e6ddd14be5d6797ae9846dd7b530ac855ffb5
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-01-24 01:42:46 +0200

    add project & delete project

--- client/src/components/Clients/ClientRow.jsx
+++ client/src/components/Clients/ClientRow.jsx
@@ -2,18 +2,20 @@ import { useMutation } from '@apollo/client';
 import { FaTrash } from 'react-icons/fa';
 import { DELETE_CLIENT } from '../../mutations/clientMutation';
 import { GET_CLIENTS } from '../../queries/clientQuery';
+import { GET_PROJECTS } from '../../queries/projectQueries';
 
 function ClientRow({ client }) {
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         variables: { id: client.id },
+        refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
         // refetchQueries: [{ query: GET_CLIENTS }],//1-variant
-        update(cache, { data: { deleteClient } }) {//доп написать в аpp cache операции
-            const { clients } = cache.readQuery({ query: GET_CLIENTS });
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: { clients: clients.filter(client => client.id !== deleteClient.id) },
-            });
-        },
+        // update(cache, { data: { deleteClient } }) {//доп написать в аpp cache операции
+        //     const { clients } = cache.readQuery({ query: GET_CLIENTS });
+        //     cache.writeQuery({
+        //         query: GET_CLIENTS,
+        //         data: { clients: clients.filter(client => client.id !== deleteClient.id) },
+        //     });
+        // },
     });
     return (
         <tr>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+
+import AddClientModal from '../components/AddClientModal'
+import AddProjectModal from '../components/AddProjectModal'
+import Clients from '../components/Clients'
+import Projects from '../components/Projects/Projects'
+
+function Home() {
+    return (
+        <>
+            <div className='d-flex gap-3 mb-4'>
+                <AddClientModal />
+                <AddProjectModal />
+            </div>
+            <Projects />
+            <hr />
+            <Clients />
+        </>
+    )
+}
+
+export default Home
