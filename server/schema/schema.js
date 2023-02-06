5c3225315699ca7d2136fbbdfd1cf3b5040e6b61
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-02-06 11:33:43 +0200

    add product

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,3 +1,5 @@
+//1-я операция написать типы nameType
+//2-я написать в RootQuery 
 // const {projects, clients} = require('../userData.js');
 
 //Mongoose models
@@ -8,6 +10,56 @@ const Client = require('../models/Client');
 // graphql.GraphQLObjectType
 
 const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull, GraphQLEnumType} = require('graphql');
+
+////////
+
+const products = [
+{
+"id": '1',
+"title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+"price": '109.95',
+"description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+"category": "men's clothing",
+"image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+
+},
+{
+"id": '2',
+"title": "Mens Casual Premium Slim Fit T-Shirts ",
+"price":' 22.3',
+"description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+"category": "men's clothing",
+"image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+
+},
+{
+"id": '3',
+"title": "Mens Cotton Jacket",
+"price": '55.99',
+"description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+"category": "men's clothing",
+"image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+
+},
+];
+
+//Movie Type
+
+const ProductType = new GraphQLObjectType({
+    name:'Product',
+    fields:()=>({
+        id:{type:GraphQLString},
+        title:{type:GraphQLString},
+        price:{type:GraphQLString},
+        description:{type:GraphQLString},
+        category:{type:GraphQLString},
+        category:{type:GraphQLString},
+        image:{type:GraphQLString},
+    }),
+});
+
+
+////////
 
 //Project Type
 const ProjectType =  new GraphQLObjectType({
@@ -41,6 +93,15 @@ const ClientType =  new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
     fields:{
+        product:{//запрос одного фильма
+            type:ProductType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){//какие данные возврашать
+             return products.find(product => product.id === args.id);
+         },
+        },
+
+
         projects:{
             type: new GraphQLList(ProjectType),
             resolve(parent,args){
